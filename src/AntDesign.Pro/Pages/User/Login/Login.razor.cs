82f81b06ed13d27b1cd8cd3ffdce85c162fc567d
hzxg <hzxg@outlook.com>
2023-09-10 19:08:06 +0800

    准备加入EF

--- src/AntDesign.Pro/Pages/User/Login/Login.razor.cs
+++ src/AntDesign.Pro/Pages/User/Login/Login.razor.cs
@@ -3,29 +3,33 @@ using AntDesign.Pro.Template.Models;
 using AntDesign.Pro.Template.Services;
 using Microsoft.AspNetCore.Components;
 using AntDesign;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Net.Http;
 
-namespace AntDesign.Pro.Template.Pages.User {
-  public partial class Login {
-    private readonly LoginParamsType _model = new LoginParamsType();
+namespace AntDesign.Pro.Template.Pages.User
+{
+    public partial class Login
+    {
+        private readonly LoginParamsType _model = new LoginParamsType();
 
-    [Inject] public NavigationManager NavigationManager { get; set; }
+        [Inject] public NavigationManager NavigationManager { get; set; }
 
-    [Inject] public IAccountService AccountService { get; set; }
+        [Inject] public IAccountService AccountService { get; set; }
 
-    [Inject] public MessageService Message { get; set; }
+        [Inject] public MessageService Message { get; set; }
+        public async void HandleSubmit()
+        {
 
-    public void HandleSubmit() {
-      if (_model.UserName == "admin" && _model.Password == "ant.design") {
-        NavigationManager.NavigateTo("/");
-        return;
-      }
+            if (_model.UserName == "admin" && _model.Password == "ant.design")
+            {
+                NavigationManager.NavigateTo("/");
+                return;
+            }
 
-      if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+            if (_model.UserName == "user" && _model.Password == "ant.design") NavigationManager.NavigateTo("/");
+        }
+
+
     }
-
-    public async Task GetCaptcha() {
-      var captcha = await AccountService.GetCaptchaAsync(_model.Mobile);
-      await Message.Success($"Verification code validated successfully! The verification code is: {captcha}");
-    }
-  }
 }
